14b9620b585da7b64d214691fc3d4caaf5c086f2
srshakil2 <rh3642921@gmail.com>
2024-12-30 02:39:46 +0600

    my applycation all done

--- src/componnets/MarathonDetails/MyApply.jsx
+++ src/componnets/MarathonDetails/MyApply.jsx
@@ -1,11 +1,20 @@
 import axios from "axios";
 import React, { useContext, useEffect, useState } from "react";
 import { MyMainContext } from "../../AuthProvider/AuthProvider";
+import {
+  Description,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+} from "@headlessui/react";
 import Swal from "sweetalert2";
 
 const MyApply = () => {
   const [participentEvent, setParticipentEvent] = useState([]);
-  const [modal, setModal] = useState(true);
+
+  // dasiyUI ulternativ
+  let [isOpen, setIsOpen] = useState(false);
+
   const { user } = useContext(MyMainContext);
   // console.log(user?.email);
   useEffect(() => {
@@ -17,14 +26,14 @@ const MyApply = () => {
       });
   }, [user, participentEvent]);
 
-  const handelUpdate = (_id) => {
-    event.preventDefault();
+  const handelUpdate = (e, _id) => {
+    e.preventDefault();
 
     console.log(_id, "update info");
 
     //
     //
-    const form = event.target;
+    const form = e.target;
     const email = form.email.value;
     const marathon_titel = form.titel.value;
     const register_date = form.registerDate.value;
@@ -43,7 +52,7 @@ const MyApply = () => {
         info: info,
       })
       .then((res) => {
-        setModal(true);
+        setIsOpen(false);
         if (res?.data?.modifiedCount === 1) {
           Swal.fire({
             title: "Update succes!",
@@ -84,7 +93,7 @@ const MyApply = () => {
     });
     console.log(_id, "this is delete");
   };
-
+  // console.log(participentEvent);
   return (
     <div className="overflow-x-auto h-screen mt-10">
       {/* register user marathon list */}
@@ -106,12 +115,14 @@ const MyApply = () => {
               <td>{item?.firstName}</td>
               <td>{item?.marathon_titel}</td>
               <tr>
+                {/* update btn table */}
                 <button
-                  onClick={() => document.getElementById(item?._id).showModal()}
+                  onClick={() => setIsOpen(true)}
                   className="btn mr-2 mb-1 text-orange-700 bg-blue-200"
                 >
                   Update
                 </button>
+                {/* delete btn table */}
                 <button
                   onClick={() => handelDelete(item?._id)}
                   className="btn mb-1 bg-orange-200 text-red-800"
@@ -122,141 +133,132 @@ const MyApply = () => {
             </tr>
 
             {/* modal Update for uniq id */}
-            <dialog
-              id={`${item?._id}`}
-              className={`modal ${modal ? "" : "hidden"}`}
+            <Dialog
+              open={isOpen}
+              onClose={() => setIsOpen(false)}
+              className="relative z-50"
             >
-              <div className="modal-box">
-                <form method="dialog">
-                  <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
-                    ✕
-                  </button>
-                </form>
-                <h1 className="text-2xl font-extrabold p-5">Update Info</h1>
-                {/* info form */}
-                <form
-                  onSubmit={() => handelUpdate(item?._id)}
-                  className=" update card-body grid md:grid-cols-2 gap-5"
-                >
-                  {/* email */}
-                  <div className="form-control">
-                    <label className="label">
-                      <span className="label-text text-lg font-semibold">
-                        Email
-                      </span>
-                    </label>
-                    <input
-                      readOnly
-                      type="text"
-                      name="email"
-                      value={item?.email}
-                      className="input input-bordered text-gray-400"
-                    />
-                  </div>
-                  {/* marathon titel */}
-                  <div className="form-control">
-                    <label className="label">
-                      <span className="label-text text-lg font-semibold">
-                        Marathon Titel
-                      </span>
-                    </label>
-                    <input
-                      readOnly
-                      type="text"
-                      value={item?.marathon_titel}
-                      name="titel"
-                      className="input input-bordered text-gray-400"
-                    />
-                  </div>
-                  {/* regiter date */}
-                  <div className="form-control">
-                    <label className="label">
-                      <span className="label-text text-lg font-semibold">
-                        Registration Date
-                      </span>
-                    </label>
-                    <input
-                      readOnly
-                      type="text"
-                      name="registerDate"
-                      value={item?.register_date}
-                      className="input input-bordered text-gray-400"
-                    />
-                  </div>
-                  {/* frist name */}
-                  <div className="form-control">
-                    <label className="label">
-                      <span className="label-text text-lg font-semibold">
-                        First Name
-                      </span>
-                    </label>
-                    <input
-                      type="text"
-                      name="first_name"
-                      defaultValue={item?.firstName}
-                      className="input input-bordered"
-                    />
-                  </div>
-                  {/* last name */}
-                  <div className="form-control">
-                    <label className="label">
-                      <span className="label-text text-lg font-semibold">
-                        Last Name
-                      </span>
-                    </label>
-                    <input
-                      type="text"
-                      name="last_name"
-                      defaultValue={item?.lastName}
-                      className="input input-bordered"
-                    />
-                  </div>
-                  {/* contact number */}
-                  <div className="form-control">
-                    <label className="label">
-                      <span className="label-text text-lg font-semibold">
-                        Contact
-                      </span>
-                    </label>
-                    <input
-                      type="number"
-                      name="contact"
-                      defaultValue={item?.contact}
-                      className="input input-bordered"
-                    />
-                  </div>
-                  {/* info */}
-                  <div className="form-control md:col-span-2">
-                    <label className="label">
-                      <span className="label-text text-lg font-semibold">
-                        Enter or Info
-                      </span>
-                    </label>
-                    <textarea
-                      className="textarea textarea-bordered"
-                      defaultValue={item?.info}
-                      name="info"
-                    ></textarea>
-                  </div>
-                  {/* btn */}
-                  <br />
-                  <div className="form-control mt-6 md:col-span-2 ">
-                    {/* ei khane kisu kaj ache modal bondho hocche na */}
-                    <button
-                      onClick={() => setModal(!true)}
-                      type="submit"
-                      className="btn text-white text-xl  bg-orange-400 hover:bg-orange-300"
-                    >
-                      Update Now
-                    </button>
-                  </div>
-                  {/* modal close */}
-                  {/* delete for modal uniq id */}
-
-                  {/*  */}
-                </form>
+              <div className="fixed inset-0 flex justify-center pt-24 mt-1 mb-10 ">
+                <DialogPanel className=" shadow-lg rounded-xl bg-white p-12 ">
+                  <form
+                    onSubmit={(e) => handelUpdate(e, item?._id)}
+                    className=" grid grid-cols-2 md:grid-cols-3 gap-5"
+                  >
+                    {/* form data */}
+                    {/* email */}
+                    <div className="form-control">
+                      <label className="label">
+                        <span className="label-text text-lg font-semibold">
+                          Email
+                        </span>
+                      </label>
+                      <input
+                        readOnly
+                        type="text"
+                        name="email"
+                        value={item?.email}
+                        className="input input-bordered text-gray-400"
+                      />
+                    </div>
+                    {/* marathon titel */}
+                    <div className="form-control">
+                      <label className="label">
+                        <span className="label-text text-lg font-semibold">
+                          Marathon Titel
+                        </span>
+                      </label>
+                      <input
+                        readOnly
+                        type="text"
+                        value={item?.marathon_titel}
+                        name="titel"
+                        className="input input-bordered text-gray-400"
+                      />
+                    </div>
+                    {/* regiter date */}
+                    <div className="form-control">
+                      <label className="label">
+                        <span className="label-text text-lg font-semibold">
+                          Registration Date
+                        </span>
+                      </label>
+                      <input
+                        readOnly
+                        type="text"
+                        name="registerDate"
+                        value={item?.register_date}
+                        className="input input-bordered text-gray-400"
+                      />
+                    </div>
+                    {/* frist name */}
+                    <div className="form-control">
+                      <label className="label">
+                        <span className="label-text text-lg font-semibold">
+                          First Name
+                        </span>
+                      </label>
+                      <input
+                        type="text"
+                        name="first_name"
+                        defaultValue={item?.firstName}
+                        className="input input-bordered"
+                      />
+                    </div>
+                    {/* last name */}
+                    <div className="form-control">
+                      <label className="label">
+                        <span className="label-text text-lg font-semibold">
+                          Last Name
+                        </span>
+                      </label>
+                      <input
+                        type="text"
+                        name="last_name"
+                        defaultValue={item?.lastName}
+                        className="input input-bordered"
+                      />
+                    </div>
+                    {/* contact number */}
+                    <div className="form-control">
+                      <label className="label">
+                        <span className="label-text text-lg font-semibold">
+                          Contact
+                        </span>
+                      </label>
+                      <input
+                        type="number"
+                        name="contact"
+                        defaultValue={item?.contact}
+                        className="input input-bordered"
+                      />
+                    </div>
+                    {/* info */}
+                    <div className="form-control md:col-span-2">
+                      <label className="label">
+                        <span className="label-text text-lg font-semibold">
+                          Enter or Info
+                        </span>
+                      </label>
+                      <textarea
+                        className="textarea textarea-bordered"
+                        defaultValue={item?.info}
+                        name="info"
+                      ></textarea>
+                    </div>
+                    {/* btn form submit */}
+                    <div className="flex gap-4 items-center justify-center mt-5">
+                      <button
+                        type="submit"
+                        // onClick={() => setIsOpen(false)}
+                        className="btn text-white text-xl  bg-orange-400 hover:bg-orange-300"
+                      >
+                        Update Now
+                      </button>
+                    </div>
+                  </form>
+                </DialogPanel>
               </div>
-            </dialog>
+            </Dialog>
             {/*  */}
           </tbody>
         ))}
